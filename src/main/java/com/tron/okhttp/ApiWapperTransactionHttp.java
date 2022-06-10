f72f1c8ede767d6e201485b5c4ca52eb8713b130
gray <xiaoguidename@gmail.com>
2022-06-10 17:54:06 +0800

    ApiWapper

--- src/main/java/com/tron/okhttp/ApiWapperTransactionHttp.java
+++ src/main/java/com/tron/okhttp/ApiWapperTransactionHttp.java
@@ -1,17 +1,17 @@
 package com.tron.okhttp;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.google.protobuf.ByteString;
 import com.tron.okhttp.utils.TronConstant;
-import com.tron.okhttp.utils.TronHttpUtils;
+import org.bouncycastle.util.encoders.Hex;
 import org.tron.trident.abi.FunctionReturnDecoder;
 import org.tron.trident.abi.TypeReference;
 import org.tron.trident.abi.datatypes.Function;
 import org.tron.trident.abi.datatypes.Utf8String;
 import org.tron.trident.core.ApiWrapper;
+import org.tron.trident.core.exceptions.IllegalException;
+import org.tron.trident.core.key.KeyPair;
 import org.tron.trident.proto.Chain;
 import org.tron.trident.proto.Response;
+import org.tron.trident.utils.Base58Check;
 import org.tron.trident.utils.Numeric;
 
 import java.util.Arrays;
@@ -19,32 +19,80 @@ import java.util.Collections;
 
 public class ApiWapperTransactionHttp {
 
-  public static void main(String[] args) throws Exception {
+  public static ApiWrapper getApiWrapper() {
+    //return ApiWrapper.ofMainnet(TronConstant.privateKey, TronConstant.apiKey);
+     return ApiWrapper.ofShasta(TronConstant.privateKey);
+  }
 
-    //ApiWrapper client = ApiWrapper.ofShasta(TronConstant.privateKey);
-    ApiWrapper client = ApiWrapper.ofMainnet(TronConstant.privateKey,TronConstant.apiKey);
+  public static void trunsferTRX() throws IllegalException, InterruptedException {
+    ApiWrapper client = getApiWrapper();
+    Response.TransactionExtention transactionExtention =
+        client.transfer(
+            "TA7weEqvrbyMygQmbNbFuAi2ndEBUAp45h", "TQaiH8b4LBFPBfd78i7mS2RZNpPWSaFx3B", 1000000L);
+    Chain.Transaction transaction = client.signTransaction(transactionExtention);
+    String txid = client.broadcastTransaction(transaction);
+    System.out.println(txid);
+
+    Thread.sleep(2000);
+
+    Chain.Transaction getTransaction = client.getTransactionById(txid);
+    System.out.println(getTransaction.getRet(0).getContractRet().name());
+  }
+
+  public static void trunsferUSDT() {
+    ApiWrapper client = getApiWrapper();
 
     Response.Account account = client.getAccount("TNzeyjDYagHSBXJYMKvAtwRBrDgGpwGrMf");
     System.out.println(account.getBalance());
 
-    Function name = new Function("name",
-            Collections.emptyList(), Arrays.asList(new TypeReference<Utf8String>() {}));
-    Response.TransactionExtention extension = client.constantCall("TNzeyjDYagHSBXJYMKvAtwRBrDgGpwGrMf", "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t", name);
+    Function name =
+        new Function(
+            "name", Collections.emptyList(), Arrays.asList(new TypeReference<Utf8String>() {}));
+    // Function name = new Function("balanceOf", Arrays.asList(new
+    // Address("TNzeyjDYagHSBXJYMKvAtwRBrDgGpwGrMf")), Arrays.asList(new TypeReference<Utf8String>()
+    // {}));
+    Response.TransactionExtention extension =
+        client.constantCall(
+            "TNzeyjDYagHSBXJYMKvAtwRBrDgGpwGrMf", "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t", name);
 
     String result = Numeric.toHexString(extension.getConstantResult(0).toByteArray());
 
-    String value = (String) FunctionReturnDecoder.decode(result, name.getOutputParameters()).get(0).getValue();
+    String value =
+        (String) FunctionReturnDecoder.decode(result, name.getOutputParameters()).get(0).getValue();
     System.out.println(result);
     System.out.println(value);
+  }
 
-    /*Response.TransactionExtention transactionExtention = client.transfer("TNzeyjDYagHSBXJYMKvAtwRBrDgGpwGrMf",
-            "TRivVnAC3BRUvhsXBbUi44yajZRxPQGPEW",
-            10000000L);
-    Chain.Transaction  transaction= client.signTransaction(transactionExtention);
+  public static void main(String[] args) throws Exception {
+    // trunsferUSDT();
+     //trunsferTRX();
+
+    createAccount();
+
+
+
+  }
+
+  public static void createAccount() throws IllegalException {
+    KeyPair keyPair = KeyPair.generate();
+
+    System.out.println(keyPair.toBase58CheckAddress());
+    System.out.println(keyPair.toPrivateKey());
+
+    ApiWrapper client = getApiWrapper();
+    Response.TransactionExtention transactionExtention =
+            client.createAccount(TronConstant.ownerAddress, keyPair.toBase58CheckAddress());
+    Chain.Transaction transaction = client.signTransaction(transactionExtention);
     String txid = client.broadcastTransaction(transaction);
-    System.out.println(txid);*/
+    System.out.println(txid);
+  }
 
-
-    //client.triggerCall()
+  /** base58 和 hex 互转 */
+  public void format() {
+    byte[] rawAddr = Hex.decode("4159d3ad9d126e153b9564417d3a05cf51c1964edf");
+    String base58 = Base58Check.bytesToBase58(rawAddr);
+    System.out.println(base58);
+    byte[] base58Byte = Base58Check.base58ToBytes(base58);
+    System.out.println(Hex.toHexString(base58Byte));
   }
 }
--- src/main/java/com/tron/okhttp/utils/TronConstant.java
+++ src/main/java/com/tron/okhttp/utils/TronConstant.java
@@ -1,7 +1,7 @@
 package com.tron.okhttp.utils;
 
 public class TronConstant {
-  public static String ownerAddress = "TNzeyjDYagHSBXJYMKvAtwRBrDgGpwGrMf";
+  public static String ownerAddress = "TA7weEqvrbyMygQmbNbFuAi2ndEBUAp45h";
   public static String apiKey = "68aea214-f593-4946-8cb2-c5ecb7e9e67f";
-  public static String privateKey = "e21b9b74f0ea0d8b82b8fab1e981afdb35332a6aef2c0ed4e19a96552a9789ce";
+  public static String privateKey = "e088f372ffaf11b33900e5fd369abdc64c4819386436541347bb4d9af17dd708";
 }
