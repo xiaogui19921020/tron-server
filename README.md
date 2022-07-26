88e4b8bb37e7a1140574d5b5e22803cd91712a19
gray <xiaoguidename@gmail.com>
2022-07-26 14:18:52 +0800

    java TRC20合约

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tron-server
- java调用波场智能合约
+- java TRC20合约
+- springboot 调用TRC20合约
+- springboot 调用波场TRC20合约
+
+1. 创建一个波场钱包，用这个钱包发TRC20合约
+2. [发行TRC20代币教程](https://cn.developers.tron.network/v4.0/docs/%E5%8F%91%E8%A1%8Ctrc20%E4%BB%A3%E5%B8%81%E6%95%99%E7%A8%8B)
+3. 替换TronConstant里参数就可以了
+
--- src/main/java/com/tron/okhttp/TronApiUtils.java
+++ src/main/java/com/tron/okhttp/TronApiUtils.java
@@ -1,80 +1,211 @@
 package com.tron.okhttp;
 
 import com.tron.okhttp.utils.TronConstant;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import org.bouncycastle.util.encoders.Hex;
 import org.tron.trident.abi.FunctionReturnDecoder;
 import org.tron.trident.abi.TypeReference;
+import org.tron.trident.abi.datatypes.Address;
+import org.tron.trident.abi.datatypes.Bool;
 import org.tron.trident.abi.datatypes.Function;
-import org.tron.trident.abi.datatypes.Utf8String;
+import org.tron.trident.abi.datatypes.generated.Uint256;
 import org.tron.trident.core.ApiWrapper;
 import org.tron.trident.core.exceptions.IllegalException;
 import org.tron.trident.core.key.KeyPair;
+import org.tron.trident.core.transaction.TransactionBuilder;
 import org.tron.trident.proto.Chain;
 import org.tron.trident.proto.Response;
 import org.tron.trident.utils.Base58Check;
 import org.tron.trident.utils.Numeric;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class TronApiUtils {
 
-  public static ApiWrapper getApiWrapper(String hexPrivateKey) {
-    //return ApiWrapper.ofMainnet(TronConstant.privateKey, TronConstant.apiKey);
-     return ApiWrapper.ofShasta(hexPrivateKey);
-  }
+  public static void main(String[] args) throws Exception {
+    String toAddress = "TXoDw9PUuBWwp2GmiDvFwdXfja6KREpkyY";
 
-  public static void trunsferTRX() throws IllegalException, InterruptedException {
-    ApiWrapper client = getApiWrapper(TronConstant.privateKey);
-    Response.TransactionExtention transactionExtention =
-        client.transfer(
-            "TA7weEqvrbyMygQmbNbFuAi2ndEBUAp45h", "TQaiH8b4LBFPBfd78i7mS2RZNpPWSaFx3B", 1000000L);
-    Chain.Transaction transaction = client.signTransaction(transactionExtention);
-    String txid = client.broadcastTransaction(transaction);
+    // TRX操作
+    /*Response.Account account = getAccount(TronConstant.privateKey, "TA7weEqvrbyMygQmbNbFuAi2ndEBUAp45h");
+    System.out.println(
+        "余额：" + new BigDecimal(account.getBalance()).divide(new BigDecimal(1000000)));
+
+    String txid1 = trunsferTRX(TronConstant.privateKey, TronConstant.ownerAddress, toAddress, 100000000L);
+    System.out.println("交易ID：" + txid1);
+    Thread.sleep(5000);
+    String txstatus1 = getTransactionStatusById(TronConstant.privateKey, txid1);
+    System.out.println("交易状态：" + txstatus1);*/
+
+    // USDT合约操作
+    BigInteger balanceOf =
+        getUSDTBalanceOf(TronConstant.privateKey, TronConstant.ownerAddress); // 查询USDT余额
+    BigDecimal bd =
+        new BigDecimal(balanceOf).divide(new BigDecimal(TronConstant.TRC20_USDT_DECIMALS));
+    // System.out.println(balanceOf);
+    System.out.println(bd);
+
+    String txid =
+        trunsferUSDT(
+            TronConstant.privateKey,
+            TronConstant.ownerAddress,
+            "TKqYgvjPthsyvinQ4Qd2cYvvXaAVzPBskY",
+            TronConstant.TRC20_USDT_DECIMALS.multiply(new BigInteger("100"))); // 转账USDT
     System.out.println(txid);
+    Thread.sleep(5000);
+    String txstatus = getTransactionStatusById(TronConstant.privateKey, txid); // 查询交易状态
+    System.out.println("交易状态：" + txstatus);
 
-    Thread.sleep(2000);
+    // String tranStr = getTransactionsTRC20(address1,true,false);
+    // System.out.println(tranStr);
 
-    Chain.Transaction getTransaction = client.getTransactionById(txid);
-    System.out.println(getTransaction.getRet(0).getContractRet().name());
   }
 
-  public static void trunsferUSDT() {
-    ApiWrapper client = getApiWrapper();
+  public static ApiWrapper getApiWrapper(String hexPrivateKey) {
+    if (TronConstant.isTest) {
+      return ApiWrapper.ofShasta(hexPrivateKey); // 测试网
+    } else {
+      return ApiWrapper.ofMainnet(hexPrivateKey, TronConstant.apiKey); // 主网
+    }
+  }
 
-    Response.Account account = client.getAccount("TNzeyjDYagHSBXJYMKvAtwRBrDgGpwGrMf");
-    System.out.println(account.getBalance());
+  /**
+   * @param address 账号
+   * @param only_to 只查询转入交易
+   * @param only_from 只查询转出交易
+   * @return
+   * @throws IOException
+   */
+  public static String getTransactionsTRC20(String address, boolean only_to, boolean only_from)
+      throws IOException {
+    OkHttpClient client = new OkHttpClient();
+    Request request =
+        new Request.Builder()
+            .url(
+                TronConstant.tronDomainUlr
+                    + "/v1/accounts/"
+                    + address
+                    + "/transactions/trc20?only_to="
+                    + only_to
+                    + "&only_from="
+                    + only_from
+                    + "&limit=100&contract_address="
+                    + TronConstant.TRC20_USDT_ADDRESS)
+            .get()
+            .addHeader("Accept", "application/json")
+            .build();
 
-    Function name =
+    okhttp3.Response response = client.newCall(request).execute();
+    return response.body().string();
+  }
+
+  /**
+   * 转账TRX
+   *
+   * @param hexPrivateKey
+   * @param fromAddress
+   * @param toAddress
+   * @param amount
+   * @return
+   * @throws IllegalException
+   */
+  public static String trunsferTRX(
+      String hexPrivateKey, String fromAddress, String toAddress, long amount)
+      throws IllegalException {
+    ApiWrapper client = getApiWrapper(hexPrivateKey);
+    Response.TransactionExtention transactionExtention =
+        client.transfer(fromAddress, toAddress, amount);
+    Chain.Transaction transaction = client.signTransaction(transactionExtention);
+    return client.broadcastTransaction(transaction);
+  }
+
+  /**
+   * 查询交易状态
+   *
+   * @param hexPrivateKey
+   * @param txid
+   * @return
+   * @throws IllegalException
+   */
+  public static String getTransactionStatusById(String hexPrivateKey, String txid)
+      throws IllegalException {
+    ApiWrapper client = getApiWrapper(hexPrivateKey);
+    Chain.Transaction getTransaction = client.getTransactionById(txid);
+    return getTransaction.getRet(0).getContractRet().name();
+  }
+
+  /**
+   * 查询账户信息
+   *
+   * @param hexPrivateKey
+   * @param address
+   * @return
+   * @throws IllegalException
+   */
+  public static Response.Account getAccount(String hexPrivateKey, String address) {
+    ApiWrapper client = getApiWrapper(hexPrivateKey);
+
+    Response.Account account = client.getAccount(address);
+    return account;
+  }
+
+  /**
+   * 获取USDT余额
+   *
+   * @param hexPrivateKey
+   * @param address
+   * @return
+   */
+  public static BigInteger getUSDTBalanceOf(String hexPrivateKey, String address) {
+    ApiWrapper client = getApiWrapper(hexPrivateKey);
+    Function balanceOf =
         new Function(
-            "name", Collections.emptyList(), Arrays.asList(new TypeReference<Utf8String>() {}));
-    // Function name = new Function("balanceOf", Arrays.asList(new
-    // Address("TNzeyjDYagHSBXJYMKvAtwRBrDgGpwGrMf")), Arrays.asList(new TypeReference<Utf8String>()
-    // {}));
+            "balanceOf",
+            Arrays.asList(new Address(address)),
+            Arrays.asList(new TypeReference<Uint256>() {}));
     Response.TransactionExtention extension =
-        client.constantCall(
-            "TNzeyjDYagHSBXJYMKvAtwRBrDgGpwGrMf", "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t", name);
+        client.constantCall(address, TronConstant.TRC20_USDT_ADDRESS, balanceOf);
 
     String result = Numeric.toHexString(extension.getConstantResult(0).toByteArray());
 
-    String value =
-        (String) FunctionReturnDecoder.decode(result, name.getOutputParameters()).get(0).getValue();
-    System.out.println(result);
-    System.out.println(value);
+    BigInteger value =
+        (BigInteger)
+            FunctionReturnDecoder.decode(result, balanceOf.getOutputParameters()).get(0).getValue();
+    return value;
   }
 
-  public static void main(String[] args) throws Exception {
-    // trunsferUSDT();
-     //trunsferTRX();
+  /**
+   * 转账USDT
+   *
+   * @param hexPrivateKey
+   * @param fromAddress
+   * @param toAddress
+   * @param amount
+   * @return
+   */
+  public static String trunsferUSDT(
+      String hexPrivateKey, String fromAddress, String toAddress, BigInteger amount) {
+    ApiWrapper client = getApiWrapper(hexPrivateKey);
+    Function transfer =
+        new Function(
+            "transfer",
+            Arrays.asList(new Address(toAddress), new Uint256(amount)),
+            Arrays.asList(new TypeReference<Bool>() {}));
 
-    //createAccount();
+    TransactionBuilder builder =
+        client.triggerCall(fromAddress, TronConstant.TRC20_USDT_ADDRESS, transfer);
+    builder.setFeeLimit(10000000);
 
-
-
+    Chain.Transaction transaction = client.signTransaction(builder.getTransaction());
+    return client.broadcastTransaction(transaction);
   }
 
   /**
    * 创建账号，并激活
+   *
    * @throws IllegalException
    */
   public static void createAccount() throws IllegalException {
@@ -83,9 +214,10 @@ public class TronApiUtils {
     System.out.println(keyPair.toBase58CheckAddress());
     System.out.println(keyPair.toPrivateKey());
 
-    ApiWrapper client = getApiWrapper();
+    ApiWrapper client = getApiWrapper(TronConstant.privateKey);
     Response.TransactionExtention transactionExtention =
-            client.createAccount(TronConstant.ownerAddress, keyPair.toBase58CheckAddress());
+        client.createAccount(TronConstant.ownerAddress, keyPair.toBase58CheckAddress());
+
     Chain.Transaction transaction = client.signTransaction(transactionExtention);
     String txid = client.broadcastTransaction(transaction);
     System.out.println(txid);
